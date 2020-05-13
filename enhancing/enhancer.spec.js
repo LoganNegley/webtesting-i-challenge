320ecaf705c7c9a7a50296d0f42c55f1b658b532
Logan Negley <logannegley@gmail.com>
2020-05-13 18:27:28 -0500

    repair test failed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,3 +1,16 @@
 const enhancer = require('./enhancer.js');
 
-
+describe('enhancer.repair()', () =>{
+    it('should restore durability to 100', () =>{
+        expect(enhancer.repair({
+            name: "knife",
+            durability: 50,
+            enhancement: 10
+        }))
+        .toBe({
+            name:"knife",
+            durability:100,
+            enhancement:10
+        })
+    })
+})
