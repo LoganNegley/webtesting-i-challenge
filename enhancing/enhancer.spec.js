b223f8ed4ecbb6f7c0a61aa2c3fc3b286ddb4431
Logan Negley <logannegley@gmail.com>
2020-05-13 19:31:09 -0500

    fail failed test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -49,3 +49,28 @@ describe('enhancer.succeed()', () =>{
         
     })
 })
+
+// Test for enhancement fail
+describe('enhancer.fail()', () =>{
+    it('shoulde return an item with durability and accasional enhancement adjustments ', () =>{
+        expect(enhance.fail({name: "knife", durability:10, enhancement: 10}))
+        .toEqual({
+            name:"knife",
+            durability:5,
+            enhancement:10
+        })
+        expect(enhancer.succeed({name:'boat', durability: 99, enhancement: 18}))
+        .toEqual({
+            name:'boat',
+            durability:89,
+            enhancement:18
+        })
+        expect(enhancer.succeed({name: 'gun', durability: 1, enhancement: 17}))
+        .toEqual({
+            name:'gun',
+            durability:1,
+            enhancement: 16
+        })
+        
+    })
+})
